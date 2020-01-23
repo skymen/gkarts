6a6f314bbf6506327821897d58305142a477f3a1
Ossama <skymen75019@gmail.com>
2020-01-23 09:15:40 +0100

    First commit

---
+++ Assets/KartController.cs
@@ -0,0 +1,321 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.Rendering.PostProcessing;
+using Cinemachine;
+using Photon.Pun;
+
+public class KartController : MonoBehaviour
+{
+    private PostProcessVolume postVolume;
+    private PostProcessProfile postProfile;
+    public PhotonView pv;
+
+    public Transform kartModel;
+    public Transform kartNormal;
+    public Rigidbody sphere;
+
+    public List<ParticleSystem> primaryParticles = new List<ParticleSystem>();
+    public List<ParticleSystem> secondaryParticles = new List<ParticleSystem>();
+
+    float speed, currentSpeed;
+    float rotate, currentRotate;
+    int driftDirection;
+    float driftPower;
+    int driftMode = 0;
+    bool first, second, third;
+    Color c;
+
+    [Header("Bools")]
+    public bool drifting;
+    public bool enabled;
+
+    [Header("Parameters")]
+
+    public float acceleration = 30f;
+    public float steering = 80f;
+    public float gravity = 10f;
+    public LayerMask layerMask;
+
+    [Header("Model Parts")]
+
+    public Transform frontWheels;
+    public Transform backWheels;
+    public Transform steeringWheel;
+    public GameObject kart;
+    public Material otherMaterial;
+
+    [Header("Particles")]
+    public Transform wheelParticles;
+    public Transform flashParticles;
+    public Color[] turboColors;
+
+    void Start()
+    {
+        postVolume = Camera.main.GetComponent<PostProcessVolume>();
+        postProfile = postVolume.profile;
+
+        for (int i = 0; i < wheelParticles.GetChild(0).childCount; i++)
+        {
+            primaryParticles.Add(wheelParticles.GetChild(0).GetChild(i).GetComponent<ParticleSystem>());
+        }
+
+        for (int i = 0; i < wheelParticles.GetChild(1).childCount; i++)
+        {
+            primaryParticles.Add(wheelParticles.GetChild(1).GetChild(i).GetComponent<ParticleSystem>());
+        }
+
+        foreach(ParticleSystem p in flashParticles.GetComponentsInChildren<ParticleSystem>())
+        {
+            secondaryParticles.Add(p);
+        }
+    }
+
+    public void SetPlayerMaterial()
+    {
+        kart.GetComponent<MeshRenderer>().material = otherMaterial;
+    }
+
+    void Update()
+    {
+        if (!enabled || !pv.IsMine) return;
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            float time = Time.timeScale == 1 ? .2f : 1;
+            Time.timeScale = time;
+        }
+
+        //Follow Collider
+        transform.position = sphere.transform.position - new Vector3(0, 0.4f, 0);
+
+        //Accelerate
+        if (Input.GetKey(KeyCode.Z))
+            speed = acceleration;
+
+        //Steer
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            int dir = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+            float amount = Mathf.Abs((Input.GetAxis("Horizontal")));
+            Steer(dir, amount);
+        }
+
+        //Drift
+        if (Input.GetButtonDown("Jump") && !drifting)
+        {
+            if (Input.GetAxis("Horizontal") != 0)
+            {
+                drifting = true;
+                driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;
+
+                GetComponent<PhotonView>().RPC("MainParticles", RpcTarget.AllViaServer);
+            }
+            
+            kartModel.parent.DOComplete();
+            kartModel.parent.DOPunchPosition(transform.up * .2f, .3f, 5, 1);
+
+        }
+
+        if (drifting)
+        {
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            float powerControl = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, .2f, 1) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 1, .2f);
+            Steer(driftDirection, control);
+            driftPower += powerControl;
+
+            ColorDrift();
+        }
+
+        if (Input.GetButtonUp("Jump") && drifting)
+        {
+            Boost();
+        }
+
+        currentSpeed = Mathf.SmoothStep(currentSpeed, speed, Time.deltaTime * 12f); speed = 0f;
+        currentRotate = Mathf.Lerp(currentRotate, rotate, Time.deltaTime * 4f); rotate = 0f;
+
+        //Animations    
+
+        //a) Kart
+        if (!drifting)
+        {
+            kartModel.localEulerAngles = Vector3.Lerp(kartModel.localEulerAngles, new Vector3(0, 90 + (Input.GetAxis("Horizontal") * 15), kartModel.localEulerAngles.z), .2f);
+        }
+        else
+        {
+            float control = (driftDirection == 1) ? ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, .5f, 2) : ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, .5f);
+            kartModel.parent.localRotation = Quaternion.Euler(0, Mathf.LerpAngle(kartModel.parent.localEulerAngles.y,(control * 15) * driftDirection, .2f), 0);
+        }
+
+        //b) Wheels
+        frontWheels.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * 15), frontWheels.localEulerAngles.z);
+        frontWheels.localEulerAngles += new Vector3(0, 0, sphere.velocity.magnitude/2);
+        backWheels.localEulerAngles += new Vector3(0, 0, sphere.velocity.magnitude/2);
+
+        //c) Steering Wheel
+        steeringWheel.localEulerAngles = new Vector3(-25, 90, ((Input.GetAxis("Horizontal") * 45)));
+
+    }
+
+    [PunRPC]
+    public void MainParticles()
+    {
+        foreach (ParticleSystem p in primaryParticles)
+        {
+            p.startColor = Color.clear;
+            p.Play();
+        }
+    }
+
+    [PunRPC]
+    public void MainParticlesStop()
+    {
+        foreach (ParticleSystem p in primaryParticles)
+        {
+            p.startColor = Color.clear;
+            p.Stop();
+        }
+    }
+
+    [PunRPC]
+    public void SecondaryParticles(Vector3 color)
+    {
+        Color c = new Color(color.x, color.y, color.z);
+        foreach (ParticleSystem p in secondaryParticles)
+        {
+            var pmain = p.main;
+            pmain.startColor = c;
+            p.Play();
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        if (!pv.IsMine) return;
+        //Forward Acceleration
+        if(!drifting)
+            sphere.AddForce(-kartModel.transform.right * currentSpeed, ForceMode.Acceleration);
+        else
+            sphere.AddForce(transform.forward * currentSpeed, ForceMode.Acceleration);
+
+        //Gravity
+        sphere.AddForce(Vector3.down * gravity, ForceMode.Acceleration);
+
+        //Steering
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, new Vector3(0, transform.eulerAngles.y + currentRotate, 0), Time.deltaTime * 5f);
+
+        RaycastHit hitOn;
+        RaycastHit hitNear;
+
+        Physics.Raycast(transform.position + (transform.up*.1f), Vector3.down, out hitOn, 1.1f,layerMask);
+        Physics.Raycast(transform.position + (transform.up * .1f)   , Vector3.down, out hitNear, 2.0f, layerMask);
+
+        //Normal Rotation
+        kartNormal.up = Vector3.Lerp(kartNormal.up, hitNear.normal, Time.deltaTime * 8.0f);
+        kartNormal.Rotate(0, transform.eulerAngles.y, 0);
+    }
+
+    [PunRPC]
+    public void BoostRPC()
+    {
+        kartModel.Find("Tube001").GetComponentInChildren<ParticleSystem>().Play();
+        kartModel.Find("Tube002").GetComponentInChildren<ParticleSystem>().Play();
+    }
+
+    public void Boost()
+    {
+        drifting = false;
+
+        if (driftMode > 0)
+        {
+            float duration = .5f * driftMode;
+            DOVirtual.Float(currentSpeed * 4, currentSpeed, duration, Speed);
+            DOVirtual.Float(0, 2, .5f, ChromaticAmount)
+                .OnComplete(() => DOVirtual.Float(2, 0, duration, ChromaticAmount));
+            GetComponent<PhotonView>().RPC("BoostRPC", RpcTarget.AllViaServer);
+        }
+
+        driftPower = 0;
+        driftMode = 0;
+        first = false; second = false; third = false;
+
+        GetComponent<PhotonView>().RPC("MainParticlesStop", RpcTarget.AllBuffered);
+
+        kartModel.parent.DOLocalRotate(Vector3.zero, .5f).SetEase(Ease.OutBack);
+
+    }
+
+    public void Steer(int direction, float amount)
+    {
+        rotate = (steering * direction) * amount;
+    }
+
+    public void ColorDrift()
+    {
+        if(!first)
+            c = Color.clear;
+
+        if (driftPower > 50 && driftPower < 100-1 && !first)
+        {
+            first = true;
+            c = turboColors[0];
+            driftMode = 1;
+
+            PlayFlashParticle(c);
+        }
+
+        if (driftPower > 100 && driftPower < 150- 1 && !second)
+        {
+            second = true;
+            c = turboColors[1];
+            driftMode = 2;
+
+            PlayFlashParticle(c);
+        }
+
+        if (driftPower > 150 && !third)
+        {
+            third = true;
+            c = turboColors[2];
+            driftMode = 3;
+
+            PlayFlashParticle(c);
+        }
+
+        foreach (ParticleSystem p in primaryParticles)
+        {
+            var pmain = p.main;
+            pmain.startColor = c;
+        }
+
+        foreach(ParticleSystem p in secondaryParticles)
+        {
+            var pmain = p.main;
+            pmain.startColor = c;
+        }
+    }
+
+    void PlayFlashParticle(Color c)
+    {
+        GameObject.Find("CM vcam1").GetComponent<CinemachineImpulseSource>().GenerateImpulse();
+
+        GetComponent<PhotonView>().RPC("SecondaryParticles", RpcTarget.AllViaServer, new Vector3(c.r, c.g, c.b));
+    }
+
+    private void Speed(float x)
+    {
+        currentSpeed = x;
+    }
+
+    void ChromaticAmount(float x)
+    {
+        postProfile.GetSetting<ChromaticAberration>().intensity.value = x;
+    }
+
+    //private void OnDrawGizmos()
+    //{
+    //    Gizmos.color = Color.red;
+    //    Gizmos.DrawLine(transform.position + transform.up, transform.position - (transform.up * 2));
+    //}
+}
---
+++ Assets/MenuController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using Cinemachine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+
+public class MenuController : MonoBehaviourPunCallbacks 
+{
+    public CinemachineVirtualCamera menuCam;
+
+    public CinemachineVirtualCamera gameCam;
+
+    public ParticleSystem particles1;
+
+    public ParticleSystem particles2;
+
+    public KartController player;
+
+    public GameObject playerGameObject;
+
+    public GameObject spawnPoint;
+    private bool connected = false;
+
+    public List<GameObject> plots;
+    private bool myRoom = false;
+
+    private void Start()
+    {
+        menuCam.gameObject.SetActive(true);
+        gameCam.gameObject.SetActive(false);
+        player.enabled = false;
+        player.SetPlayerMaterial();
+        particles1.Play();
+        particles2.Play();
+        PhotonNetwork.AddCallbackTarget(this);
+        Debug.Log(spawnPoint);
+        Connect();
+    }
+
+    public void Play()
+    {
+        Debug.Log("Play");
+        if (!connected) return;
+        Debug.Log("Connected");
+        menuCam.gameObject.SetActive(false);
+        gameCam.gameObject.SetActive(true);
+        player.enabled = true;
+        particles1.Stop();
+        particles2.Stop();
+        Debug.Log("Trying to join room...");
+        Connect();
+    }
+    
+    public override void OnConnected()
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnected() was called by PUN");
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        connected = true;
+        Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+        myRoom = true;
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Destroy(playerGameObject);
+        playerGameObject = PhotonNetwork.Instantiate(Path.Combine("Player"), spawnPoint.transform.position, spawnPoint.transform.rotation, 0);
+        player = playerGameObject.GetComponentInChildren<KartController>();
+        player.enabled = true;
+        player.SetPlayerMaterial();
+        gameCam.Follow = player.transform;
+        gameCam.LookAt = player.transform;
+        Debug.Log("Player instantiated");
+    }
+    
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = "1";
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
